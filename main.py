77ea893b89e349cf74a1361ac586de49c774b61f
jawsubak <acandiatale@gmail.com>
2021-02-28 11:59:05 +0900

    init

---
+++ main.py
@@ -0,0 +1,27 @@
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+app = Ursina()
+
+window.fps_counter.enabled = False
+# window.exit_button.visible = False
+
+class Voxel(Button):
+    def __init__(self, position=(0, 0, 0), texture='brick'):
+        super().__init__(
+            parent=scene,
+            position=position,
+            model='cube',
+            origin_y=0.5,
+            texture=texture,
+            color=color.color(0, 0, random.uniform(0.9, 1.0)),
+            scale=1.0
+        )
+
+for z in range(20):
+    for x in range(20):
+        voxel = Voxel(position=(x, 0, z))
+
+player = FirstPersonController()
+
+app.run()
